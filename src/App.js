38d94683a1b3be146179c24d46ffb643267b39f1
CJ <79920294+ChristianJohnRoa07@users.noreply.github.com>
2023-12-06 15:46:44 +0800

    Changed UI Loading Screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { React, lazy, useEffect, useState, Suspense } from "react";
 
 import "./App.css";
+import "./loadingCSS.css";
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
-import { fadeIn } from "./Components/Variants";
+
 
 const Homepage = lazy(() => import("./Components/Homepage"));
 
-const PropagateLoader = lazy(() => import("react-spinners/PropagateLoader"));
+
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -15,51 +16,31 @@ const App = () => {
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
-    }, 3000);
+    }, 5000);
   }, []);
 
   return (
     <div>
       <AnimatePresence>
-        {/* {loading ? (
+        {loading ? (
           <motion.div
             className="grid place-items-center h-screen "
             key="loader"
             initial="show"
           >
-            <PropagateLoader
-              color="#00acc1"
-              loading={loading}
-              size={15}
-              aria-label="Loading Spinner"
-              data-testid="loader"
-            />
+            <div className="flex gap-7">
+              <span className="font-poppins font-bold text-6xl text-cyan-500">
+                CJ
+              </span>
+              <span class="loader"></span>
+              <span className="font-poppins font-bold text-6xl text-cyan-500">
+                DR
+              </span>
+            </div>
           </motion.div>
         ) : (
           <Homepage />
-        )} */}
-
-        <Suspense
-          fallback={
-             <motion.div
-          className="flex flex-col justify-center items-center gap-2 h-screen "
-          key="loader"
-          initial="show"
-        >
-          <span className="font-poppins font-[700] text-center text-cyan-200 text-6xl">CJDR.</span>
-
-          <PropagateLoader
-            color="#00acc1"
-            
-            size={20}
-            aria-label="Loading Spinner"
-            data-testid="loader"
-          />
-        </motion.div>
-          }
-        >
-          <Homepage />
-        </Suspense>
+        )}
       </AnimatePresence>
     </div>
   );
