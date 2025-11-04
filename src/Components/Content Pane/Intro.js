ff26d844f8eec293a6760e1c3efef72b4a01bb97
Christian John Roa <roacj.cr@gmail.com>
2025-11-04 13:06:25 +0800

    Update intro message

--- src/Components/Content Pane/Intro.js
+++ src/Components/Content Pane/Intro.js
@@ -5,17 +5,13 @@ const Intro = () => {
     <div className="px-20 md:px-10 lg:px-24 pt-[10%] md:pt-[180px] lg:pt-[20%]">
       <div className=" text-justify ">
         <span className="font-poppins text-sm  md:text-lg text-gray-500 ">
-          A <span className="text-black dark:text-white">highly motivated</span>{" "}
-          and <span className="text-black dark:text-white">dedicated</span>{" "}
-          Computer Science graduate with a strong background in front-end
-          technologies and a passion for developing web apps or softwares.
-          Seeking an entry-level opportunity related to{" "}
-          <span className="text-black dark:text-white">Web Development</span> or{" "}
-          <span className="text-black dark:text-white">
-            Software Development
-          </span>
-          . Eager to grow and further develop skills while contributing to
-          innovative projects and the company.
+
+          A Computer Science graduate with a strong foundation in <span className="text-black dark:text-white">front-end and back-end development</span>, 
+          specialized in <span className="text-black dark:text-white">developing responsive and
+          efficient web applications enhancing user experience and streamline
+          business processes</span>. Seeking an opportunity to apply skills in innovative
+          projects, grow professionally, and contribute meaningfully to the
+          success of the company.
         </span>
       </div>
     </div>
