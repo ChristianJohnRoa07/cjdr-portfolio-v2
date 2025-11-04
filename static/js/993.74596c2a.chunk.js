3a58825657c24b20143d937e8b345c4bf9df3ef0
Christian John Roa <roacj.cr@gmail.com>
2025-11-04 12:41:04 +0800

    Updates

---
+++ static/js/993.74596c2a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcjdr_portfolio_v2=self.webpackChunkcjdr_portfolio_v2||[]).push([[993],{7993:function(n,e,t){t.r(e);var r=t(2791),u=t(184),c=(0,r.lazy)((function(){return t.e(247).then(t.bind(t,2247))})),i=(0,r.lazy)((function(){return t.e(537).then(t.bind(t,537))})),l=(0,r.lazy)((function(){return t.e(949).then(t.bind(t,949))}));e.default=function(){return(0,u.jsxs)("div",{className:"flex flex-col gap-20",children:[(0,u.jsx)(c,{}),(0,u.jsx)(i,{}),(0,u.jsx)(l,{})]})}}}]);
+//# sourceMappingURL=993.74596c2a.chunk.js.map
